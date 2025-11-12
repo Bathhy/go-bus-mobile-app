19037fd06a7e8fe2f9f19dd557c102902babd33a
endsy2 <kongming434@gmail.com>
2025-11-12 23:52:23 +0700

    home feature

--- go_bus_express/lib/main.dart
+++ go_bus_express/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:go_bus_express/resources/routes/app_routes.dart';
+import 'package:go_bus_express/view/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,12 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      initialRoute: AppRoutes.selectRoute,
-      getPages: AppRoutes.routes,
+      home: HomePage(),
     );
   }
 }
