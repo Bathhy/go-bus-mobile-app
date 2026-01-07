29aab4f4d7421eb26f3f3ea4da05ac04f49a406f
Thong Vathana <thongvathana22@gmail.com>
2026-01-07 15:41:03 +0700

    fetch-ticket-api

--- go_bus_express/lib/view_models/controller/ticket/ticket_state.dart
+++ go_bus_express/lib/view_models/controller/ticket/ticket_state.dart
@@ -5,12 +5,21 @@ class TicketState extends BaseUiState {
   final String type;
   final List<TicketModel> tickets;
 
-  TicketState({this.type = "coming", this.tickets = const []});
+  TicketState({
+    this.type = "coming", 
+    this.tickets = const [],
+    super.isLoading = false,
+  });
 
-  TicketState copyWith({String? type, List<TicketModel>? tickets}) {
+  TicketState copyWith({
+    String? type, 
+    List<TicketModel>? tickets,
+    bool? isLoading,
+  }) {
     return TicketState(
       type: type ?? this.type,
       tickets: tickets ?? this.tickets,
+      isLoading: isLoading ?? this.isLoading,
     );
   }
 }
